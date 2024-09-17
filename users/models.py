429a11c1c4e583090a62d053277c78d2e3252c8e
mmdhasan <mohammad.hasan.najafii.2004@gmail.com>
2024-09-17 17:10:54 +0330

    fixed problem that the images were not showing -> edited users:urls.py

--- users/models.py
+++ users/models.py
@@ -6,11 +6,11 @@ class User(AbstractUser):
     pic = models.ImageField(_("Profile Picture"), upload_to="images/profile_pics", height_field=None, width_field=None, max_length=None, blank=True, null=True)
     groups = models.ManyToManyField(
         Group,
-        related_name='custom_user_set',  # Change the related name to avoid clash
+        related_name='custom_user_set',
         blank=True,
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name='custom_user_permissions_set',  # Change the related name to avoid clash
+        related_name='custom_user_permissions_set',
         blank=True,
     )
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import AccountSettingsView
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'users'
 
 urlpatterns = [
     path('account_settings/', AccountSettingsView.as_view(), name="account_settings")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
