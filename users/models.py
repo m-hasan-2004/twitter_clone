f50b92daf9504ab0f73c95f349d902960bb09d9d
mmdhasan <mohammad.hasan.najafii.2004@gmail.com>
2024-09-17 17:06:08 +0330

    added user model pic attr + admin.py of user created

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext as _
 from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class User(AbstractUser):
-    pic = models.ImageField(_("Profile Picture"), upload_to="images/profile_pics", height_field=None, width_field=None, max_length=None)
+    pic = models.ImageField(_("Profile Picture"), upload_to="images/profile_pics", height_field=None, width_field=None, max_length=None, blank=True, null=True)
     groups = models.ManyToManyField(
         Group,
         related_name='custom_user_set',  # Change the related name to avoid clash
