003618be12b4fda906c13d499e396fa2c24ef7bd
mmdhasan <mohammad.hasan.najafii.2004@gmail.com>
2024-09-18 15:20:50 +0330

    users model id changed to uuid4 auto generate

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext as _
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from uuid import uuid4
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     pic = models.ImageField(_("Profile Picture"), upload_to="images/profile_pics", height_field=None, width_field=None, max_length=None, blank=True, null=True)
     groups = models.ManyToManyField(
         Group,
