9b6ceb37ad698bf7003b8a68cfbcbd437f7614b1
mmdhasan <mohammad.hasan.najafii.2004@gmail.com>
2024-09-22 19:25:20 +0330

    added profile pic choosing on signup + profile pic deletion on del acc

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,9 +16,10 @@ class ProfileForm(forms.ModelForm):
         }
         
 class CustomUserCreationForm(UserCreationForm):
+    pic = forms.ImageField(label="Profile Picture", required=False)
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'pic']
         
 class AuthenticationForm(AuthenticationForm):
     class Meta:
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
-from django.contrib.auth import login, logout
+from django.conf import settings
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic.edit import CreateView
@@ -9,8 +10,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import update_session_auth_hash
 from django.contrib import messages
 from .forms import PasswordChangeForm
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.views import View
+import os
 
 
 class SignupView(CreateView):
@@ -19,11 +21,11 @@ class SignupView(CreateView):
     success_url = reverse_lazy('users:account_settings')
 
     def form_valid(self, form):
-        user = form.save()
+        user = form.save(commit=False)
+        user.pic = form.cleaned_data.get('pic')
+        user.save()
         login(self.request, user)
         return super().form_valid(form)
-
-# accounts/views.py
 
 
 class CustomLoginView(LoginView):
@@ -32,7 +34,6 @@ class CustomLoginView(LoginView):
     success_url = reverse_lazy('admin/')
 
 
-# accounts/views.py
 class AccountSettingsView(LoginRequiredMixin, FormView):
     template_name = 'users/account_settings.html'
     success_url = reverse_lazy('account_settings')
@@ -58,6 +59,12 @@ class DeleteAccountView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         user = request.user
+        if user.pic and os.path.isfile(os.path.join(settings.MEDIA_ROOT, str(user.pic))):
+            try:
+                # Remove the profile picture from the filesystem
+                os.remove(os.path.join(settings.MEDIA_ROOT, str(user.pic)))
+            except Exception as e:
+                messages.error(request, f"Error deleting profile picture: {str(e)}")
         user.delete()
         messages.success(request, "Your account has been deleted successfully.")
         return redirect(self.success_url)
