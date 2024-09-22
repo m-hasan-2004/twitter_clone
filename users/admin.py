fafebd12bcd0da98f75139acc82e6031cad5bae8
mmdhasan <mohammad.hasan.najafii.2004@gmail.com>
2024-09-22 20:35:08 +0330

    added more functionality to users/admin

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,24 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'last_login')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
+    readonly_fields = ('id', 'last_login', 'date_joined')
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'email', 'pic')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Important Dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+    save_on_top = True
+    ordering = ('-date_joined', 'username')
+    list_per_page = 25  
