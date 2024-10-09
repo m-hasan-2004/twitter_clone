ebc307a3561b13bc46a0c2fc00bfe511c69d220f
mmdhasan <mohammad.hasan.najafii.2004@gmail.com>
2024-10-09 19:02:59 +0330

    refactor: deleted useless js code

--- static/js/base.js
+++
@@ -1,7 +0,0 @@
-// static/js/base.js
-
-// Function to toggle the mobile menu
-document.getElementById('hamburger').addEventListener('click', function () {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-});
