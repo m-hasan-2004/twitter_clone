96d16a7cccd735d3f69ec2c01cb456b647564985
hasan <mohammad.hasan.najafii.2004@gmail.com>
2024-12-29 22:53:27 +0330

    fix: fixed the problem about profile pic in templates

--- twitter/settings.py
+++ twitter/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .email_token import Password
+# from .email_token import Password
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -137,12 +137,12 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:account_settings'
 LOGIN_URL = "users:login"
 
-# Config For Sending Email Using Django Send mail (Gmail smtp backend)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = Password.EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = Password.EMAIL_HOST_PASSWORD
+# # Config For Sending Email Using Django Send mail (Gmail smtp backend)
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = Password.EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = Password.EMAIL_HOST_PASSWORD
 
-handler404 = "core.views.custom_404"
+# handler404 = "core.views.custom_404"
